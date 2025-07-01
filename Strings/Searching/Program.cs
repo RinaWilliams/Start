ac160af8db647e1b16ccb894d6cfa8f9be940103
serena <rinawilliams2116@gmail.com>
2025-07-01 16:25:07 +0100

    searching

--- Strings/Searching/Program.cs
+++ Strings/Searching/Program.cs
@@ -3,17 +3,28 @@
 
 string teststr = "The quick brown Fox jumps over the lazy Dog";
 
-// TODO: Contains determines whether a string contains certain content
+//  Contains determines whether a string contains certain content
+Console.WriteLine($"{teststr.Contains("fox")}");
+Console.WriteLine($"{teststr.Contains("fox",StringComparison.CurrentCultureIgnoreCase)}");
 
-
-// TODO: StartsWith and EndsWith determine if a string starts 
+//  StartsWith and EndsWith determine if a string starts 
 // or ends with a given test string
+Console.WriteLine($"{teststr.StartsWith("the")}");
+Console.WriteLine($"{teststr.StartsWith("the",StringComparison.CurrentCultureIgnoreCase)}");
+Console.WriteLine($"{teststr.EndsWith("dog")}");
+Console.WriteLine($"{teststr.EndsWith("dog",StringComparison.CurrentCultureIgnoreCase)}");
 
+// IndexOf, LastIndexOf finds the index of a substring
+Console.WriteLine($"{teststr.IndexOf("the")}");
+Console.WriteLine($"{teststr.IndexOf("the",
+StringComparison.CurrentCultureIgnoreCase)}");
+Console.WriteLine($"{teststr.LastIndexOf("the")}");
 
-// TODO: IndexOf, LastIndexOf finds the index of a substring
+// Determining empty, null, or whitespace
 
-
-// TODO: Determining empty, null, or whitespace
 string str1 = null;
 string str2 = "   ";
 string str3 = String.Empty;
+Console.WriteLine($"{String.IsNullOrEmpty(str1)}");
+Console.WriteLine($"{String.IsNullOrEmpty(str3)}");
+Console.WriteLine($"{String.IsNullOrWhiteSpace(str2)}");
